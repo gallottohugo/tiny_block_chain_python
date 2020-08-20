463da2a112e73c3f10eacd0ae033ce797da79c57
gallottohugo <gallottohugo@gmail.com>
2020-08-20 09:20:42 -0300

    Refact

--- bchain_web_interface.py
+++ bchain_web_interface.py
@@ -1,18 +1,18 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 from flask import jsonify
 from block_chain import BlockChain, Transaction
 
 osaCoin=BlockChain()
-osaCoin.createTransaction(Transaction('address1','address2',100))
-osaCoin.createTransaction(Transaction('address2','address1',50))
+osaCoin.createTransaction(Transaction('address1', 'address2', 100))
+osaCoin.createTransaction(Transaction('address2', 'address1', 50))
 
 app = Flask(__name__)
 
-@app.route("/mine",methods=['GET'])
+@app.route("/mine", methods=['GET'])
 def mine():
-    return "we are going to mine the block with new transations here"
+    return "We are going to mine the block with new transations here"
 
-@app.route('/transactions/new',methods=['POST'])
+@app.route('/transactions/new', methods=['POST'])
 def new_transation():
     return None
 
@@ -20,7 +20,7 @@ def new_transation():
 def hello():
     if request.method == "POST":
         osaCoin.minePendingTransaction("osamaaddress")
-    return  render_template('index.html',pending_transactions=osaCoin.pendingTransations)
+    return  render_template('index.html', pending_transactions=osaCoin.pendingTransations)
 
 if __name__=="__main__":
     app.run(debug=True)
